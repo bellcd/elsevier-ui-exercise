160e4606c569d4eb425a092a999ee46d7665a6c2
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2020-03-27 04:27:49 -0400

    Improves Patients component a11y

--- src/components/patients.js
+++ src/components/patients.js
@@ -4,7 +4,7 @@ import { PATIENTS } from '../sample-data';
 export const Patients = ({ changeActivePatient }) => {
   const patientList = PATIENTS.map(patient => {
     return <ul key={patient.id}>
-      <a onClick={changeActivePatient} href="#"><li data-id={patient.id}>{patient.name}</li></a>
+      <button className="patient-button" onClick={changeActivePatient} ><li data-id={patient.id}>{patient.name}</li></button>
     </ul>
   });
   return patientList;
