192cc08cc1ac627bcf26cc5d82c1c06bd1ecc251
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2020-03-29 22:19:16 -0400

    Adds documentation for utils

--- src/utils.js
+++ src/utils.js
@@ -6,23 +6,45 @@ const options = {
   }
 }
 
-// TODO: add proper comments
-
 export default {
-  // get a patient's info
+  /**
+   *
+   * Get a patient's demographic info
+   * @param {String} patientId
+   * @returns {Promise}
+   */
   getPatientInfo: (patientId) => {
     return fetch(`${config.serviceRootUrl}/Patient/${patientId}`, options);
   },
 
-  // fetch the conditions for a particular person
+  /**
+   *
+   * Get conditions for a particular patient
+   * @param {String} patientId
+   * @returns {Promise}
+   */
   getConditions: (patientId) => {
     return fetch(`${config.serviceRootUrl}/Condition/?patient=${patientId}`, options);
   },
 
+  /**
+   *
+   * Filters an array to only active conditions
+   * @param {Array} conditionsList
+   * @returns {Array}
+   */
   filterToActiveConditions: (conditionsList) => {
     return conditionsList.filter((condition) => condition.resource.clinicalStatus === 'active');
   },
 
+
+  /**
+   *
+   * Returns a new object with the activePatientId & isLoading arguments given, as well as an error property set to null
+   * @param {String} activePatientId
+   * @param {Boolean} isLoading
+   * @returns {Object}
+   */
   updateActivePatient: (activePatientId, isLoading) => {
     return {
       activePatientId,
@@ -31,6 +53,15 @@ export default {
     }
   },
 
+  /**
+   *
+   * Returns a new object with the patient and condition arguments given merged at the activePatientId argument key of the patients & conditions properties, respectively
+   * @param {Object} state - the state object
+   * @param {String} activePatientId
+   * @param {Object} patient - patient demographic info
+   * @param {Object} conditions - contains a list of conditions for this patient
+   * @returns {Object}
+   */
   updatePatientsAndConditions: (state, activePatientId, patient, conditions) => {
     return {
       patients: {...state.patients, ...{ [activePatientId]: patient }},
@@ -40,6 +71,13 @@ export default {
     }
   },
 
+  /**
+   *
+   * Returns a boolean on whether or not the existing state object has information  (ie, both patients & conditions) for a particular activePatientId
+   * @param {Object} state
+   * @param {String} activePatientId
+   * @returns {Boolean}
+   */
   havePatientAndConditions: (state, activePatientId) => {
     return !!(state.patients[activePatientId] && state.conditions[activePatientId]);
   }
